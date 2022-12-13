f5199f005e4f3f608546b1de632ad92fcfc42dee
xxxsjan <626653354@qq.com>
2022-12-13 20:41:24 +0800

    更改base

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from 'D:/HEELOWORLD/front-end/my-git/webpack5-docs/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("D:/HEELOWORLD/front-end/my-git/webpack5-docs/webpack_docs/node_modules/@vuepress/theme-default/lib/client/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("D:/HEELOWORLD/front-end/my-git/webpack5-docs/webpack_docs/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
-}
