7eba81eb8b8184bc5ee7b1bc5042f91309be8bc7
xxxsjan <626653354@qq.com>
2022-12-13 20:12:14 +0800

    first commit

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'D:/HEELOWORLD/front-end/my-git/webpack5-docs/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
